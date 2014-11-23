ed2be89233b1f231213b2d2044ca38c782f54a20
Antokolos <antokolos@gmail.com>
2014-11-23 20:48:29 +0600

    Some JS corrections

--- www/pglua.js
+++ www/pglua.js
@@ -1,38 +1,19 @@
 var Lua = {
-    isRun: false,
     isInitialized: false,
     state: null,
     tmp_id: 0,
     default_source_name: 'stdin',
-    preallocated_strings: {
-        '__handle': null,
-        '__index': null,
-    },
     initialize: function (source_name, stdout, stderr) {
         if (this.isInitialized) throw new Error('Lua already initialized');
         this.default_source_name = source_name || this.default_source_name;
         this.stdout = stdout || this.stdout;
         this.stderr = stderr || this.stderr;
-
-        if (!this.isRun) {
-            run();
-            this.isRun = true;
-        }
-        this.state = _luaL_newstate();
-        _luaL_openlibs(this.state);
-        for (var key in this.preallocated_strings) {
-            this.preallocated_strings[key] = this.allocate_string(key);
-        }
+        this.state = _lua_initialize();
         this.isInitialized = true;
     },
     destroy: function() {
         if (!this.isInitialized) throw new Error('Lua is not initialized');
         _lua_close(this.state);
-
-        /*for (var i = 0; i < Runtime.functionPointers.length; i++) {
-            Runtime.functionPointers[i] = null;
-        }*/
-
         this.isInitialized = false;
     },
     stdout: function (str) {console.log("stdout: " +str)},
@@ -62,14 +43,7 @@ var Lua = {
     },
     inject: function (object, name, final_location, metatable) {
         name = name || this.get_tmp_name();
-        this.pushStack(object);
-        if (metatable) {
-            this.pushStack(metatable);
-            _lua_setmetatable(this.state, -2);
-        }
-        var strptr = this.allocate_string(name);
-        _lua_setglobal(this.state, strptr);
-        _free(strptr);
+        this._lua_inject(object, name, metatable);
         if (final_location) {
             this.exec(final_location + " = " + name + "\n" + name + " = nil");
         }
@@ -101,119 +75,41 @@ var Lua = {
 Lua.cache['items'] = {};
 Lua.cache['clear'] = function (evalstring) { delete Lua.cache['items'][evalstring] };
 Lua.get_tmp_name = function() { return "_weblua_tmp_" + this.tmp_id++; };
-Lua.pushStack = pushStack: function(object) {
-    if (object === null) {
-        object = undefined;
-    }
-    switch(typeof object) {
-        case "undefined" :
-            _lua_pushnil(this.state);
-            return 1;
-        case "boolean" :
-            _lua_pushboolean(this.state, object);
-            return 1;
-        case "number" :
-            _lua_pushnumber(this.state, object);
-            return 1;
-        case "string" :
-            var strptr = this.allocate_string(object);
-            _lua_pushstring(this.state, strptr);
-            _free(strptr);
-            return 1;
-        case "function" :
-            var self = this;
-            var wrapper = function (state) {
-                var result = object.apply(self, self.get_stack_args());
-                if (result == undefined || result == null) {
-                    result = [];
-                }
-                if (!( typeof result == 'object' && typeof result.length == "number")) {
-                    throw new Error("Expected array return type from JS function");
-                }
-                for (var i = 0; i < result.length; i++) {
-                    self.pushStack(result[i]);
-                }
-                return result.length;
-            }
-            wrapper.unwrapped = object;
-            var pointer = Runtime.addFunction(wrapper);
-            _lua_pushcclosure(this.state, pointer, 0);
-            return 1;
-        case "object" :
-            if (object.length === undefined) {
-                // Object
-                _lua_createtable(this.state, 0, 0);
-                if (object['__handle']) {
-                    // Handled object
-                    var source = object;
-                    var metatable = {
-                        '__index': function (table, key) {
-                            return [source[key]];
-                        },
-                        '__newindex': function (table, key, value) {
-                            source[key] = value;
-                            return [];
-                        },
-                    }
-                    metatable['__index'].source = source;
-
-                    this.pushStack(metatable);
-                    _lua_setmetatable(this.state, -2);
-
-                    object = {'__handle': object.toString()};
-                }
-                for (var k in object) {
-                    this.pushStack(k);
-                    this.pushStack(object[k]);
-                    _lua_rawset(this.state, -3);
-                }
-            } else {
-                // Array
-                _lua_createtable(this.state, object.length, 0);
-                for (var k in object) {
-                    k = 1*k;
-                    this.pushStack(k+1)
-                    this.pushStack(object[k]);
-                    _lua_rawset(this.state, -3);
-                }
-            }
-            return 1;
-        default:
-            throw new Error("Cannot push object to stack: " + object);
-    }
-};
-Lua.allocate_string = function(str) {
-    // TODO: Add implementation
-    return null;
-    //var arr = intArrayFromString(str);
-    //return allocate(arr, 'i8', 0);  // ALLOC_NORMAL
-};
-Lua.run = function() {
+Lua._lua_initialize = function() {
     cordova.exec(
         function() {}, // success callback function
         function() {}, // error callback function
         'PhoneGapLua', // mapped to our native Java class called "CalendarPlugin"
-        'run', // with this action name
+        '_lua_initialize', // with this action name
         [{                  // and this array of custom arguments to create our entry
 
         }]
     );
-};
-Lua._luaL_newstate = function() {
-    // TODO: Add implementation
     return null;
 };
-Lua._luaL_openlibs = function(state) {};
-Lua._lua_close = function(state) {};
-Lua._lua_setmetatable = function(state, n) {};
-Lua._lua_setglobal= function(state, n) {};
-Lua,_free = function(strptr) {};
-Lua._lua_pushnil = function(state) {};
-Lua._lua_pushboolean = function(state, object) {};
-Lua._lua_pushnumber = function(state, object) {};
-Lua._lua_pushstring = function(state, strptr) {};
-Lua._lua_pushcclosure = function(state, pointer, n) {};
-Lua._lua_createtable = function(state, n, m) {};
-Lua._lua_rawset = function(state, n) {};
+Lua._lua_close = function() {
+    cordova.exec(
+        function() {}, // success callback function
+        function() {}, // error callback function
+        'PhoneGapLua', // mapped to our native Java class called "CalendarPlugin"
+        '_lua_close', // with this action name
+        [{                  // and this array of custom arguments to create our entry
+
+        }]
+    );
+    return null;
+};
+Lua._lua_inject = function() {
+    cordova.exec(
+        function() {}, // success callback function
+        function() {}, // error callback function
+        'PhoneGapLua', // mapped to our native Java class called "CalendarPlugin"
+        '_lua_inject', // with this action name
+        [{                  // and this array of custom arguments to create our entry
+
+        }]
+    );
+    return null;
+};
 
 module.exports = Lua;
