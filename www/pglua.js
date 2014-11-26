49e370008da127bd91217bb057f65e0d55eb09e0
Antokolos <antokolos@gmail.com>
2014-11-26 22:56:47 +0600

    Bugfixing

--- www/pglua.js
+++ www/pglua.js
@@ -1,31 +1,32 @@
 var Lua = function() {
+    var self = this;
     var isInitialized = false;
     var tmp_id = 0;
     var default_source_name = 'stdin';
 
     this.initialize = function (source_name, stdout, stderr) {
-        if (this.isInitialized) throw new Error('Lua already initialized');
-        this.default_source_name = source_name || this.default_source_name;
-        this.stdout = stdout || this.stdout;
-        this.stderr = stderr || this.stderr;
-        this._lua_initialize();
-        this.isInitialized = true;
+        if (self.isInitialized) throw new Error('Lua already initialized');
+        self.default_source_name = source_name || self.default_source_name;
+        self.stdout = stdout || self.stdout;
+        self.stderr = stderr || self.stderr;
+        self._lua_initialize();
+        self.isInitialized = true;
     };
     this.destroy = function() {
-        if (!this.isInitialized) throw new Error('Lua is not initialized');
-        this._lua_close();
-        this.isInitialized = false;
+        if (!self.isInitialized) throw new Error('Lua is not initialized');
+        self._lua_close();
+        self.isInitialized = false;
     };
     this.stdout = function (str) {console.log("stdout: " +str)};
     this.stderr = function (str) {console.log("stderr: " +str)};
     this.eval = function (successCallback, errorCallback, command, source_name, source) {
-        source_name = source_name || this.default_source_name;
+        source_name = source_name || self.default_source_name;
         source      = source      || command;
-        this.exec(successCallback, errorCallback, "return " + command, source_name, source);
+        self.exec(successCallback, errorCallback, "return " + command, source_name, source);
     };
     this.exec = function (successCallback, errorCallback, command, source_name, source) {
-        if (!this.isInitialized) throw new Error('Lua is not initialized');
-        this._lua_exec(successCallback, errorCallback, command, source_name, source);
+        if (!self.isInitialized) throw new Error('Lua is not initialized');
+        self._lua_exec(successCallback, errorCallback, command, source_name, source);
     };
     this.anon_lua_object = function (object) {
         // Create anonymous Lua object or literal from JS object
@@ -37,26 +38,26 @@ var Lua = function() {
                 return '"' + object.replace('"','\\"') + '"';
             case "function":
             case "object":
-                return this.inject(object);
+                return self.inject(object);
             default:
                 return object.toString();
         }
     };
     this.inject = function (object, name, final_location, metatable) {
-        name = name || this.get_tmp_name();
-        this._lua_inject(object, name, metatable);
+        name = name || self.get_tmp_name();
+        self._lua_inject(object, name, metatable);
         if (final_location) {
-            this.exec(final_location + " = " + name + "\n" + name + " = nil");
+            self.exec(final_location + " = " + name + "\n" + name + " = nil");
         }
         return (final_location || name);
     };
     this.cache = function (evalstring) {
-        if (!(evalstring in this.cache['items'])) {
-            this.cache['items'][evalstring] = this.eval(evalstring)
+        if (!(evalstring in self.cache['items'])) {
+            self.cache['items'][evalstring] = self.eval(evalstring)
         }
-        return this.cache['items'][evalstring];
+        return self.cache['items'][evalstring];
     };
-    this.get_tmp_name = function() { return "_weblua_tmp_" + this.tmp_id++; };
+    this.get_tmp_name = function() { return "_weblua_tmp_" + self.tmp_id++; };
     this._lua_initialize = function() {
         cordova.exec(
             function() {}, // success callback function
